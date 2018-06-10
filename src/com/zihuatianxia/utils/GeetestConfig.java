ed02a9a2f48acd608561bcb6b721481b45efb57f
ilineicry <leileijava@126.com>
2018-06-11 06:53:17 +0800

    Update GeetestConfig.java

--- src/com/zihuatianxia/utils/GeetestConfig.java
+++ src/com/zihuatianxia/utils/GeetestConfig.java
@@ -10,8 +10,8 @@ import javax.swing.text.StyledEditorKit.BoldAction;
 public class GeetestConfig {
 
 	// 填入自己的captcha_id和private_key
-	private static final String geetest_id = "e58155133ccca9e2f7ecc54ff51a6992";
-	private static final String geetest_key = "e607b67a51b97cbcad8b0c0396b5f82e";
+	private static final String geetest_id = "";
+	private static final String geetest_key = "";
 	private static final boolean newfailback = true;
 
 	public static final String getGeetest_id() {
